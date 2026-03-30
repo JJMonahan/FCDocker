9a5bbe1c507917b62defc6910656624bb54e5ede
John Monahan <john.monahan@smartbear.com>
2026-03-30 12:50:52 -0400

    fixed / vs no slash in paths

--- fc/entrypoint.sh
+++ fc/entrypoint.sh
@@ -11,6 +11,6 @@ python manage.py migrate
 echo "------------------ showmigrations -----------------------"
 python manage.py showmigrations
 echo "------------------ collect static ------------------------"
-python manage.py collectstatic --no-input
+python manage.py collectstatic --noinput
 echo "------------------ light 'er up!  ------------------------"
 gunicorn fc.wsgi:application --bind 0.0.0.0:8000 --config gunicorn_config.py
